284045ab6bcb25341bbb0c7134cdfa203bb4dd05
irsalsss <muhamadirsal@gmail.com>
2021-10-20 06:41:45 +0700

    redirect using context

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
 import { Row, Button, Card, Typography } from "antd";
+import { useMainContext } from '../context/MainContext';
 const { Title } = Typography;
 
 const NotFound = () => {
-  const history = useHistory();
-
+  const { onRedirect } = useMainContext();
   return (
     <Row className="center-container" align="middle" justify="center">
       <Card style={{ width: 500 }}>
         <Title level={2}>Page is not found</Title>
-        <Button type="primary" htmlType="submit" onClick={() => history.push('/')}>
+        <Button type="primary" htmlType="submit" onClick={() => onRedirect()}>
           Back to mainpage
         </Button>
       </Card>
