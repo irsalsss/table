59c0f1fe22c6d39d39101bff20afc47a1fc13d0c
irsalsss <muhamadirsal@gmail.com>
2021-10-20 15:52:42 +0700

    integrate user api

--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext, createContext, useCallback } from 'react';
 import { useHistory, useLocation } from "react-router-dom";
+import { getUsers } from '../client/MainApi';
 import { errorNotif, successNotif } from '../utils/Utils';
 
 const MainContext = createContext(null);
@@ -8,27 +9,67 @@ export const MainProvider = (props) => {
   const history = useHistory();
   const { pathname } = useLocation();
 
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [filterGender, setFilterGender] = useState('all');
+  const [currentPage, setCurentPage] = useState(1);
+
   const onSearch = (e) => {
-    console.log(e.target.value)
+    setSearchKeyword(e.target.value);
   }
 
-  const onFilterGender = (e) => {
-    console.log(e)
-  }
+  const onFilterGender = useCallback((gender) => {
+    setFilterGender(gender);
+  }, [])
+
+  const onResetFilter = useCallback(() => {
+    setSearchKeyword('')
+    setFilterGender('all')
+  }, [])
 
   const onRedirect = useCallback((url = "/") => {
     history.push(url);
   }, [])
 
-  useEffect(() => {
-
+  const onChangePage = useCallback((page) => {
+    setCurentPage(page);
   }, [])
+
+  const _getUsers = async() => {
+    setIsLoading(true);
+    try {
+      const params = {
+        page: currentPage,
+        gender: filterGender === 'all' ? null : filterGender,
+        keyword: searchKeyword || null,
+      }
+      const { data } = await getUsers(params);
+      if (data.results.length) {
+        setUsers(data.results);
+      }
+    } catch (error) {
+      console.error('users-error', error)
+      errorNotif('Users | Something went wrong')
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    _getUsers();
+  }, [currentPage, filterGender])
   
   return (
     <MainContext.Provider 
       {...props}
       value={{
-        onSearch, onRedirect, onFilterGender,
+        users,
+        isLoading,
+        searchKeyword, 
+        filterGender, onFilterGender,
+        onSearch, onRedirect,
+        onResetFilter,
+        currentPage, onChangePage,
       }}
     />
   )
